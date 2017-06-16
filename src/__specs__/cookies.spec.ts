fc8fabb5c8bb9552644f8dc999d6cf1050a43a15
Lapanti <laurilavanti@gmail.com>
2017-06-16 11:22:11 +0300

    Test both browser and server environments

--- src/__specs__/cookies.spec.ts
+++ src/__specs__/cookies.spec.ts
@@ -1,25 +1,7 @@
 import cookies, { CookieError } from '../cookies';
 
-describe('Cookies', () => {
+test('Cookies', () => {
     describe('get', () => {
-        /**
-         * @jest-environment node
-         */
-        it('should throw an error when document is not defined', () => {
-            expect(() => cookies.get('doesntmatter')).toThrow(
-                'Document is not defined! Are you trying to use this on the server?',
-            );
-            expect(() => cookies.get('doesntmatter')).toThrowError(CookieError);
-        });
-
-        /**
-         * @jest-environment node
-         */
-        it('should return null if silent is enabled and document is not defined', () => {
-            expect(() => cookies.get('doesntmatter', { silent: true })).not.toThrow();
-            expect(cookies.get('doesntmatter', { silent: true })).toEqual(null);
-        });
-
         it('should return the correct value', () => {
             document.cookie = 'test=value;path=/';
             expect(cookies.get('test')).toEqual('value');
@@ -30,24 +12,6 @@ describe('Cookies', () => {
     });
 
     describe('set', () => {
-        /**
-         * @jest-environment node
-         */
-        it('should throw an error when document is not defined', () => {
-            expect(() => cookies.set('doesntmatter', '0')).toThrow(
-                'Document is not defined! Are you trying to use this on the server?',
-            );
-            expect(() => cookies.set('doesntmatter', '0')).toThrowError(CookieError);
-        });
-
-        /**
-         * @jest-environment node
-         */
-        it('should return false if silent is enabled and document is not defined', () => {
-            expect(() => cookies.set('doesntmatter', '0', { silent: true })).not.toThrow();
-            expect(cookies.set('doesntmatter', '0', { silent: true })).toBeFalsy();
-        });
-
         it('should succeed in setting a new cookie', () => {
             expect(cookies.set('test', 'value')).toBeTruthy();
             expect(cookies.get('test')).toEqual('value');
@@ -55,9 +19,48 @@ describe('Cookies', () => {
     });
 
     describe('remove', () => {
-        /**
-         * @jest-environment node
-         */
+        it('should return true on successful removal', () => {
+            document.cookie = 'test=value';
+            expect(cookies.remove('test')).toBeTruthy();
+            expect(cookies.get('test')).toEqual('');
+        });
+    });
+});
+
+/**
+ * @jest-environment node
+ */
+
+test('Cookies in server', () => {
+    describe('get', () => {
+        it('should throw an error when document is not defined', () => {
+            expect(() => cookies.get('doesntmatter')).toThrow(
+                'Document is not defined! Are you trying to use this on the server?',
+            );
+            expect(() => cookies.get('doesntmatter')).toThrowError(CookieError);
+        });
+
+        it('should return null if silent is enabled and document is not defined', () => {
+            expect(() => cookies.get('doesntmatter', { silent: true })).not.toThrow();
+            expect(cookies.get('doesntmatter', { silent: true })).toEqual('');
+        });
+    });
+
+    describe('set', () => {
+        it('should throw an error when document is not defined', () => {
+            expect(() => cookies.set('doesntmatter', '0')).toThrow(
+                'Document is not defined! Are you trying to use this on the server?',
+            );
+            expect(() => cookies.set('doesntmatter', '0')).toThrowError(CookieError);
+        });
+
+        it('should return false if silent is enabled and document is not defined', () => {
+            expect(() => cookies.set('doesntmatter', '0', { silent: true })).not.toThrow();
+            expect(cookies.set('doesntmatter', '0', { silent: true })).toBeFalsy();
+        });
+    });
+
+    describe('remove', () => {
         it('should throw an error when document is not defined', () => {
             expect(() => cookies.remove('doesntmatter')).toThrow(
                 'Document is not defined! Are you trying to use this on the server?',
@@ -65,18 +68,9 @@ describe('Cookies', () => {
             expect(() => cookies.remove('doesntmatter')).toThrowError(CookieError);
         });
 
-        /**
-         * @jest-environment node
-         */
         it('should return false if silent is enabled and document is not defined', () => {
             expect(() => cookies.remove('doesntmatter', { silent: true })).not.toThrow();
             expect(cookies.remove('doesntmatter', { silent: true })).toBeFalsy();
-        });
-
-        it('should return true on successful removal', () => {
-            document.cookie = 'test=value';
-            expect(cookies.remove('test')).toBeTruthy();
-            expect(cookies.get('test')).toEqual('');
         });
     });
 });
