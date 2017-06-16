fd62eac3671e3464852153f6ad804cf052bc3bc6
Lapanti <laurilavanti@gmail.com>
2017-06-16 10:22:08 +0300

    Initial tests with get set and remove cookies

--- src/__specs__/cookies.spec.ts
+++ src/__specs__/cookies.spec.ts
@@ -1,5 +1,82 @@
-import cookies from '../cookies';
+import cookies, { CookieError } from '../cookies';
 
 describe('Cookies', () => {
-    it('should fail', () => expect(1).not.toEqual(0));
+    describe('get', () => {
+        /**
+         * @jest-environment node
+         */
+        it('should throw an error when document is not defined', () => {
+            expect(() => cookies.get('doesntmatter')).toThrow(
+                'Document is not defined! Are you trying to use this on the server?',
+            );
+            expect(() => cookies.get('doesntmatter')).toThrowError(CookieError);
+        });
+
+        /**
+         * @jest-environment node
+         */
+        it('should return null if silent is enabled and document is not defined', () => {
+            expect(() => cookies.get('doesntmatter', { silent: true })).not.toThrow();
+            expect(cookies.get('doesntmatter', { silent: true })).toEqual(null);
+        });
+
+        it('should return the correct value', () => {
+            document.cookie = 'test=value;path=/';
+            expect(cookies.get('test')).toEqual('value');
+        });
+
+        it('should return null when searching for nonexisting cookies', () =>
+            expect(cookies.get('nonexistent')).toEqual(''));
+    });
+
+    describe('set', () => {
+        /**
+         * @jest-environment node
+         */
+        it('should throw an error when document is not defined', () => {
+            expect(() => cookies.set('doesntmatter', '0')).toThrow(
+                'Document is not defined! Are you trying to use this on the server?',
+            );
+            expect(() => cookies.set('doesntmatter', '0')).toThrowError(CookieError);
+        });
+
+        /**
+         * @jest-environment node
+         */
+        it('should return false if silent is enabled and document is not defined', () => {
+            expect(() => cookies.set('doesntmatter', '0', { silent: true })).not.toThrow();
+            expect(cookies.set('doesntmatter', '0', { silent: true })).toBeFalsy();
+        });
+
+        it('should succeed in setting a new cookie', () => {
+            expect(cookies.set('test', 'value')).toBeTruthy();
+            expect(cookies.get('test')).toEqual('value');
+        });
+    });
+
+    describe('remove', () => {
+        /**
+         * @jest-environment node
+         */
+        it('should throw an error when document is not defined', () => {
+            expect(() => cookies.remove('doesntmatter')).toThrow(
+                'Document is not defined! Are you trying to use this on the server?',
+            );
+            expect(() => cookies.remove('doesntmatter')).toThrowError(CookieError);
+        });
+
+        /**
+         * @jest-environment node
+         */
+        it('should return false if silent is enabled and document is not defined', () => {
+            expect(() => cookies.remove('doesntmatter', { silent: true })).not.toThrow();
+            expect(cookies.remove('doesntmatter', { silent: true })).toBeFalsy();
+        });
+
+        it('should return true on successful removal', () => {
+            document.cookie = 'test=value';
+            expect(cookies.remove('test')).toBeTruthy();
+            expect(cookies.get('test')).toEqual('');
+        });
+    });
 });
--- src/cookies.ts
+++ src/cookies.ts
@@ -1,7 +1,46 @@
+export class CookieError extends Error {}
+
+const year = 365;
+const daysToMillis = 24 * 60 * 60 * 1000; // tslint:disable-line:no-magic-numbers
+
+interface ICookieOptions {
+    readonly silent: boolean;
+    readonly days?: number;
+}
+
+const defaultCookieOpts: ICookieOptions = {
+    silent: false,
+    days: year,
+};
+
+const checkCookieSupport = (opts: ICookieOptions): boolean => {
+    if (typeof document === 'undefined' && !opts.silent) {
+        throw new CookieError('Document is not defined! Are you trying to use this on the server?');
+    }
+    return typeof document !== 'undefined';
+};
+
+const write = (name: string, value: string, opts: ICookieOptions = defaultCookieOpts): boolean => {
+    if (checkCookieSupport(opts)) {
+        const date = new Date();
+        const expires = !!opts.days ? `; expires=${date.setTime(date.getTime() + opts.days * daysToMillis)}` : '';
+        document.cookie = `${encodeURIComponent(name)}=${value}${expires}`;
+        return true;
+    }
+    return false;
+};
+
 const cookies = {
-    get: () => {},
-    set: () => {},
-    remove: () => {},
+    get: (name: string, opts: ICookieOptions = defaultCookieOpts): string | null => {
+        if (checkCookieSupport(opts)) {
+            const res = document.cookie.match(`(?:^|; )${encodeURIComponent(name)}=([^;]*)`);
+            return res ? res[1] : '';
+        }
+        return '';
+    },
+    set: write,
+    remove: (name: string, opts: ICookieOptions = defaultCookieOpts): boolean =>
+        write(name, '', Object.assign({ days: -1 }, opts)),
 };
 
 export default cookies;
