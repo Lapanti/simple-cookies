55ce57de569bc65d7f8b346e1218e12ffdb5dc3e
Lapanti <laurilavanti@gmail.com>
2017-06-19 14:08:10 +0300

    (#2): Working bundling

---
+++ webpack.config.ts
@@ -0,0 +1,27 @@
+import * as path from 'path';
+import * as webpack from 'webpack';
+
+module.exports = {
+    context: __dirname,
+    entry: path.resolve('src', 'cookies.ts'),
+    output: {
+        filename: 'main.js',
+        path: path.resolve('lib'),
+        library: 'simple-cookies',
+        libraryTarget: 'umd',
+        umdNamedDefine: true,
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: ['babel-loader', 'awesome-typescript-loader'],
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+};
